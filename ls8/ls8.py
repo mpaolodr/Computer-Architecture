d90a6759810b88c7b3049a8e235302b706ce6903
mpaolodr <mpaolodr_1504@yahoo.com>
2020-06-16 16:32:45 -0400

    refactored ls8.py to accept files from cmd line

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,25 @@
 """Main."""
 
 import sys
+from os import listdir
 from cpu import *
 
 cpu = CPU()
+# create a dictionary of files in examples folder
+files = {f: None for f in listdir("examples")}
 
-cpu.load()
-cpu.run()
+# if filename is provided in cmd line
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+    # check if filename provided is valid
+    if filename in files:
+        cpu.load(filename)
+    else:
+        print(f"Invalid Filename")
+        sys.exit(1)
+
+
+else:
+    # load print8 as default file
+    cpu.load("print8.ls8")
